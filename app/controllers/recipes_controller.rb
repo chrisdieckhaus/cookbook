2ff249fff7fc4a21d2fc6dfa1adbdf483481a926
Chris <christopherdieckhaus@gmail.com>
2015-06-04 13:11:55 -0400

    Form validation

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,22 @@
 class RecipesController < ApplicationController
+	def index
+		@recipes = Recipe.all
+	end
+
 	def show
 		@recipe = Recipe.find(params[:id])
 	end
-	
+
 	def new
 	end
 
 	def create
 		@recipe = Recipe.new(recipe_params)
-		@recipe.save
-		redirect_to @recipe
+		if @recipe.save
+			redirect_to @recipe
+		else
+			render 'new'
+		end
 	end
 
 	private
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ActiveRecord::Base
+	validates :title, presence: true
+	validates :ingredients, presence: true
+	validates :directions, presence: true
 end
