1e1f537cf3c149a1734697e25b6e58620d537b9e
Chris <christopherdieckhaus@gmail.com>
2015-06-04 15:32:13 -0400

    Authentication for add/edit/delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+	http_basic_authenticate_with name: "chris", password: "lovewhatyoudo", except: [:index, :show]
+	
 	def index
 		@recipes = Recipe.all
 	end
