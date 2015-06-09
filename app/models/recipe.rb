0040a2c579eea73c1ff013e8f9c6090bca049d6f
Chris <christopherdieckhaus@gmail.com>
2015-06-09 14:08:40 -0400

    Review dependency upon Recipe deletion

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
-	has_many :reviews
+	has_many :reviews, dependent: :destroy
 	validates :title, presence: true
 	validates :ingredients, presence: true
 	validates :directions, presence: true
