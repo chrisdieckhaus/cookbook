965e59b73f5e176f2bb7147ba636a85e85ad76ec
Chris <christopherdieckhaus@gmail.com>
2016-06-23 17:00:56 -0600

    Fine-tune search and allow for category-specific search

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,8 @@ class CategoriesController < ApplicationController
 	def show
 		@category = params[:id]
 		@recipes = Recipe.where(:category => @category)
+		if params[:search]
+			@recipes = Recipe.where(:category => @category).search(params[:search])
+		end
 	end
 end
