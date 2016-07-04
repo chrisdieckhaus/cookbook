f0be6151bd9fdf08d4a0581a9c17522a1f29f166
Chris <christopherdieckhaus@gmail.com>
2016-07-04 17:39:02 -0600

    Run migration for Users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609191751) do
+ActiveRecord::Schema.define(version: 20160704233355) do
 
   create_table "recipes", force: true do |t|
     t.string   "title"
@@ -33,5 +33,14 @@ ActiveRecord::Schema.define(version: 20150609191751) do
   end
 
   add_index "reviews", ["recipe_id"], name: "index_reviews_on_recipe_id"
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
