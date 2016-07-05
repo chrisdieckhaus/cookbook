0dc9d238d0e06dfc266571429c9cde5d1a2788ad
Chris <christopherdieckhaus@gmail.com>
2016-07-04 18:24:02 -0600

    User login and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
 
-  helper_method :sort_direction
+  	helper_method :sort_direction
+  	helper_method :current_user
+  
+	def current_user
+  		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  	end
 
-  private
+  	def require_user 
+		redirect_to '/login' unless current_user 
+	end
+
+	private
 		def sort_direction
 			%w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
 		end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,10 @@ Rails.application.routes.draw do
   get 'signup'  => 'users#new' 
   resources :users
 
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
