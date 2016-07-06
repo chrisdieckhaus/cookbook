2c0750b263136a08e938e77197ff1662f9e521ca
Chris <christopherdieckhaus@gmail.com>
2016-07-05 23:11:42 -0600

    List recipes reviewed by user on user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		@reviews = Review.where("user_id = ?", @user.id).order(rating: :desc)
 	end
 
 	private
